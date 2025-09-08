df2b2071a66d40639e2964f4a434b40346a1c877
Yash-Surya-Fintech <shrivastava.y@surya-fintech.com>
2025-09-08 22:53:13 +0530

    add input validation

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -4,18 +4,39 @@
     {
         static void Main(string[] args)
         {
+            int a, b;
+            string operation;
             Console.WriteLine("enter A");
-            int a=Convert.ToInt32(Console.ReadLine());
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+                Console.Write("Enter A ");
+            }
             Console.WriteLine("enter B");
-            int b=Convert.ToInt32(Console.ReadLine());
+            while (!int.TryParse(Console.ReadLine(), out b))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+                Console.Write("Enter B ");
+            }
+       
             Console.WriteLine("tell us about the operation ");
-            char op=Convert.ToChar(Console.ReadLine());
-            switch (op)
+            while (operation != "+" && operation != "-" && operation != "*" && operation != "/")
+            {
+                Console.WriteLine("Invalid operation. Please enter one of the following: +, -, *, /");
+                Console.Write("Enter an operation (+, -, *, /): ");
+                operation = Console.ReadLine();
+            }
+            switch (operation)
             {
                 case '+': Console.WriteLine(a + b); break;
                 case '-': Console.WriteLine(a - b); break;
                 case '*':Console.WriteLine(a * b); break;
-                case '/':Console.WriteLine(a / b); break;   
+                case '/':
+                    if(b==0)
+                        Console.WriteLine("No can't be divided by zero ")
+                            else 
+                        Console.WriteLine(a / b); 
+                       break;   
             }
         }
     }
